fa3938c5df09faa58b176710eda282d144fd5a3f
jhonatan182 <jona.vand98@gmail.com>
2022-07-30 14:00:14 -0600

    modificacione ene l CaloriasDao

--- calories_logger_be/dao/mongodb/models/CaloriasDao.js
+++ calories_logger_be/dao/mongodb/models/CaloriasDao.js
@@ -1,75 +1,71 @@
 const { db } = require('../Connection');
 const DaoObject = require('../DaoObject');
 module.exports = class CaloriasDao extends DaoObject {
-  constructor(db = null) {
-    super(db, 'calories_lost');
-  }
-  async setup() {
-    if (process.env.MONGODB_SETUP) {
-     
+    constructor(db = null) {
+        super(db, 'calories_lost');
     }
-  }
-
-  obtenerTodos() {
-    return this.find();
-  }
-
-  async obtenerTodosPaginacion({ userId, page = 1, pageLimit = 25 }) {
-    const calorias = await this.find(
-      { userId: this.objectId(userId) },
-      null,
-      null,
-      null,
-      null,
-      true
-    );
-    const totalDocs = await calorias.count();
-    calorias.skip(pageLimit * (page - 1))
-    calorias.limit(pageLimit);
-    const caloriasDocs = await calorias.toArray();
-    
-    return {
-      total: totalDocs,
-      page,
-      pageLimit,
-      totalPages: Math.ceil(totalDocs / pageLimit),
-      calorias: caloriasDocs
+    async setup() {
+        if (process.env.MONGODB_SETUP) {
+        }
     }
-  }
 
-  obtenerPorTipoEjercicio({ userId }) {
-    const match = {
-      '$match': {
-        userId: this.objectId(userId)
+    obtenerTodos() {
+        return this.find();
     }
-    }
-    const groupBy = {
-      '$group': {
-        _id: '$tipoEjercicio',
-        registros: { '$sum': 1 },
-        calorias: { '$sum': '$calorias' },
-        promedio: {'$avg' : '$calorias'}
 
-      }
-    }
-    const sort = {
-      '$sort': {
-        _id: -1
-      }
-    }
-    return this.aggregate([match, groupBy, sort]);
-  }
+    async obtenerTodosPaginacion({ userId, page = 1, pageLimit = 25 }) {
+        const calorias = await this.find(
+            { userId: this.objectId(userId) },
+            null,
+            null,
+            null,
+            null,
+            true
+        );
+        const totalDocs = await calorias.count();
+        calorias.skip(pageLimit * (page - 1));
+        calorias.limit(pageLimit);
+        const caloriasDocs = await calorias.toArray();
 
-  insertar({ userId, calorias, tipoEjercicio, descripcion}) {
-    const caloria = {
-      userId: this.objectId(userId),
-      fecha: new Date().toISOString(), 
-      calorias, 
-      tipoEjercicio, 
-      descripcion,  
+        return {
+            total: totalDocs,
+            page,
+            pageLimit,
+            totalPages: Math.ceil(totalDocs / pageLimit),
+            calorias: caloriasDocs,
+        };
     }
-    return super.insertOne(caloria);
-  }
 
+    obtenerPorTipoEjercicio({ userId }) {
+        const match = {
+            $match: {
+                userId: this.objectId(userId),
+            },
+        };
+        const groupBy = {
+            $group: {
+                _id: userId,
+                registros: { $sum: 1 },
+                calorias: { $sum: '$calorias' },
+                promedio: { $avg: '$calorias' },
+            },
+        };
+        const sort = {
+            $sort: {
+                _id: -1,
+            },
+        };
+        return this.aggregate([match, groupBy, sort]);
+    }
 
-}
+    insertar({ userId, calorias, tipoEjercicio, descripcion }) {
+        const caloria = {
+            userId: this.objectId(userId),
+            fecha: new Date().toISOString(),
+            calorias,
+            tipoEjercicio,
+            descripcion,
+        };
+        return super.insertOne(caloria);
+    }
+};
